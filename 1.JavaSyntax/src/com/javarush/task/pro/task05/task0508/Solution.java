c6daec07bf53f00ad4c2c7f4745c6a2dc367dea7
whiteburn <erunda995@yandex.ru>
2026-02-17 23:38:49 +0500

    commit 06

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0508/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0508/Solution.java
@@ -13,18 +13,22 @@ public class Solution {
         Scanner input = new Scanner(System.in);
         strings = new String[6];
         String temp = "";
+
         for (int i = 0; i < strings.length; i++) {
             strings[i] = input.nextLine();
         }
 
         for (int i = 0; i < strings.length; i++) {
             temp = strings[i];
-                for (int j = 0; j < strings.length; j++) {
-                    if (temp == strings[j]) {
-                        strings[j] = null;
-                    }
+
+            for (int j = 0; j < strings.length; j++) {
+                if (temp != null && temp.equals(strings[j]) && i != j){
+                    strings[i] = null;
+                    strings[j] = null;
                 }
+            }
         }
+
 
         for (int i = 0; i < strings.length; i++) {
             System.out.print(strings[i] + ", ");
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0511/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.pro.task05.task0511;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/* 
+Создаем двумерный массив
+*/
+
+public class Solution {
+    public static int[][] multiArray;
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+//        int[] array = new int[n];
+//        for (int i = 0; i < n; i++){
+//            array[i] = input.nextInt();
+//        }
+//        multiArray = new int[n][];
+//        for (int i = 0; i < multiArray.length; i++) {
+//            multiArray[i] = new int[array[i]];
+//        }
+
+        multiArray = new int[n][];
+
+        for (int i = 0; i < multiArray.length; i++) {
+            multiArray[i] = new int[input.nextInt()];
+            System.out.println(Arrays.toString(multiArray[i]));
+        }
+        System.out.println(Arrays.toString(multiArray));
+    }
+}
