24167f3dc459aaae0633e33c461d1810b61304e5
whiteburn <erunda995@yandex.ru>
2026-02-21 17:50:32 +0500

    commit 10

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String str01 = new String("01");
+        String str02 = new String("02");
+        String str03 = new String("03");
+        String str04 = new String("04");
+        String str05 = new String("05");
+        String str06 = new String("06");
+        String str07 = new String("07");
+        String str08 = new String("08");
+        String str09 = new String("09");
+        String str10 = new String("10");
+
+        int[] arr1 = new int[1];
+        int[] arr2 = new int[2];
+        int[] arr3 = new int[3];
+        int[] arr4 = new int[4];
+        int[] arr5 = new int[5];
+
+        Scanner sc1 = new Scanner(System.in);
+        Scanner sc2 = new Scanner(System.in);
+
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0802/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task08.task0802;
+
+/* 
+Утильный класс: часть 2
+*/
+
+public class Solution {
+
+    public static double sqrt(double a) {
+        return Math.sqrt(a);
+    }
+
+    public static double cbrt(double a) {
+        return Math.cbrt(a);
+    }
+
+    public static double pow(int number, int power) {
+
+        double n = (double) number;
+        double p = (double) power;
+
+
+        return Math.pow(n, p);
+    }
+}
