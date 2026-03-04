24ddac5d1352823e77bd923b4539be19f4284061
whiteburn <erunda995@yandex.ru>
2026-03-04 11:00:13 +0500

    commit 17

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1512/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.pro.task15.task1512;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.StreamCorruptedException;
+import java.util.Scanner;
+
+/* 
+Задом наперед
+*/
+
+public class Solution {
+    public static ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    public static PrintStream stream = new PrintStream(outputStream);
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        printSomething(scanner.nextLine());
+        String result = outputStream.toString();
+        outputStream.reset();
+        StringBuilder sb = new StringBuilder(result);
+        sb.reverse();
+        String reverse = sb.toString();
+        printSomething(reverse);
+        System.out.println(outputStream.toString());
+    }
+
+    public static void printSomething(String str) {
+        stream.print(str);
+    }
+}
