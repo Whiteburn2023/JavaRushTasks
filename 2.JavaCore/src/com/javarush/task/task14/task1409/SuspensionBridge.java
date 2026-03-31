8e2d6f5a409e761ef28852c59b4db470cddc0dfd
whiteburn <erunda995@yandex.ru>
2026-03-31 22:54:19 +0500

    commit 29

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1409/SuspensionBridge.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task14.task1409;
+
+public class SuspensionBridge implements Bridge{
+
+    final int CARSCOUNT = 7;
+
+    @Override
+    public int getCarsCount() {
+        return CARSCOUNT;
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task14/task1409/WaterBridge.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task14.task1409;
+
+public class WaterBridge implements Bridge{
+
+    final int CARSCOUNT = 5;
+
+    @Override
+    public int getCarsCount() {
+        return CARSCOUNT;
+    }
+}
