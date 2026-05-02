f524fd60b6b22bfde846ca1cb06ff3bd23e8f40a
whiteburn <erunda995@yandex.ru>
2026-05-02 21:32:24 +0500

    commit 45

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1807/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task18.task1807;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Подсчет запятых
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+        FileInputStream fis = new FileInputStream(console.readLine())){
+            int count = 0;
+            int data;
+            while((data = fis.read()) != -1){
+                if (data == ','){
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+    }
+}
--- 2.JavaCore/src/com/javarush/task/task18/task1808/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1808/Solution.java
@@ -7,7 +7,31 @@ import java.io.*;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+             ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+            String fileName1 = console.readLine();
+            String fileName2 = console.readLine();
+            String fileName3 = console.readLine();
 
+            try (FileInputStream fis = new FileInputStream(fileName1);
+                 FileOutputStream fos1 = new FileOutputStream(fileName2);
+                 FileOutputStream fos2 = new FileOutputStream(fileName3)) {
+
+                byte[] buffer = new byte[8192];
+                int bytesRead;
+                while ((bytesRead = fis.read(buffer)) != -1) {
+                    baos.write(buffer, 0, bytesRead);
+                }
+
+                byte[] allBytes = baos.toByteArray();
+                int length = allBytes.length;
+                int half = (length + 1) / 2;
+
+                fos1.write(allBytes, 0, half);
+                fos2.write(allBytes, half, length - half);
+
+            }
+        }
     }
 }
