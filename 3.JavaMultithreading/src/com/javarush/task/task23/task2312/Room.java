f5100618064aa1698564bd1e18c72111cdf77356
whiteburn <erunda995@yandex.ru>
2026-05-09 00:15:43 +0500

    commit 51

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Room.java
@@ -0,0 +1,79 @@
+package com.javarush.task.task23.task2312;
+
+
+public class Room {
+
+    private int width;
+    private int height;
+    private Snake snake;
+    private Mouse mouse;
+
+    public static Room game;
+
+    public static void main(String[] args){
+
+        Snake snake = new Snake(0, 0);
+        game = new Room(20, 20, snake);
+        snake.setDirection(SnakeDirection.DOWN);
+        game.createMouse();
+        game.run();
+    }
+
+    public void sleep(){
+
+    }
+
+    public void createMouse(){
+       mouse = new Mouse(((int)(Math.random() * getWidth())), (((int)(Math.random() * getHeight()))));
+    }
+
+    public void eatMouse(){
+        createMouse();
+    }
+
+    public void run(){
+
+    }
+
+    public void print(){
+
+    }
+
+    public Room(int width, int height, Snake snake) {
+        this.width = width;
+        this.height = height;
+        this.snake = snake;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Snake getSnake() {
+        return snake;
+    }
+
+    public void setSnake(Snake snake) {
+        this.snake = snake;
+    }
+
+    public Mouse getMouse() {
+        return mouse;
+    }
+
+    public void setMouse(Mouse mouse) {
+        this.mouse = mouse;
+    }
+}
