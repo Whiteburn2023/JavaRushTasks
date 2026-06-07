297324f5e4ea6af477c77272db9a520bf61bfb97
whiteburn <erunda995@yandex.ru>
2026-06-08 02:01:19 +0500

    commit 68

--- 3.JavaMultithreading/src/com/javarush/task/sql/task08/task0801/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task08/task0801/Solution.java
@@ -8,8 +8,24 @@ import java.sql.*;
 
 public class Solution {
 
-    public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+    private static final String URL = "jdbc:mysql://localhost:3306/test";
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "root";
 
+    public static void main(String[] args) throws Exception {
+        try (Connection connection = DriverManager.getConnection(URL,USER_NAME,PASSWORD)){
+            connection.setAutoCommit(false);
+            try (Statement statement = connection.createStatement()){
+                int updateSalaryDiego = statement.executeUpdate("UPDATE employee SET salary = salary + 2000 WHERE name = 'Diego'");
+                int updateSalaryAmigo = statement.executeUpdate("UPDATE employee SET salary = salary + 500 WHERE name = 'Amigo'");
+
+                connection.commit();
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/sql/task08/task0803/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task08/task0803/Solution.java
@@ -10,6 +10,10 @@ task0803
 
 public class Solution {
 
+    private static final String URL = "jdbc:mysql://localhost:3306/test";
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "root";
+
     public static void main(String[] args) throws Exception {
         String name = "Harry";
         int age = 18;
